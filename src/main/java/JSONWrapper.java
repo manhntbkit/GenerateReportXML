738cbea5f4d8a35e4bd9cec2e851114ecc0a0318
Manh Nguyen <manhnt.bkit@gmail.com>
2022-06-23 18:06:48 +0700

    init - generate simple report type ( name, format, reportType, currency).

---
+++ src/main/java/JSONWrapper.java
@@ -0,0 +1,223 @@
+public class JSONWrapper {
+    private String id;
+    private String name;
+    private String date_entered;
+    private String date_modified;
+    private String modified_user_id;
+    private String modified_by_name;
+    private String created_by;
+    private String created_by_name;
+    private String description;
+    private boolean deleted;
+    private String module;
+    private String report_type;
+    private String content;
+    private boolean is_published;
+    private String last_run_date;
+    private String report_cache_id;
+    private String chart_type;
+    private String schedule_type;
+    private boolean favorite;
+    private boolean my_favorite;
+    private String sync_key;
+    private String assigned_user_id;
+    private String assigned_user_name;
+    private String team_count;
+
+
+    // Getter Methods
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate_entered() {
+        return date_entered;
+    }
+
+    public String getDate_modified() {
+        return date_modified;
+    }
+
+    public String getModified_user_id() {
+        return modified_user_id;
+    }
+
+    public String getModified_by_name() {
+        return modified_by_name;
+    }
+
+    public String getCreated_by() {
+        return created_by;
+    }
+
+    public String getCreated_by_name() {
+        return created_by_name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean getDeleted() {
+        return deleted;
+    }
+
+    public String getModule() {
+        return module;
+    }
+
+    public String getReport_type() {
+        return report_type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public boolean getIs_published() {
+        return is_published;
+    }
+
+    public String getLast_run_date() {
+        return last_run_date;
+    }
+
+    public String getReport_cache_id() {
+        return report_cache_id;
+    }
+
+    public String getChart_type() {
+        return chart_type;
+    }
+
+    public String getSchedule_type() {
+        return schedule_type;
+    }
+
+    public boolean getFavorite() {
+        return favorite;
+    }
+
+    public boolean getMy_favorite() {
+        return my_favorite;
+    }
+
+    public String getSync_key() {
+        return sync_key;
+    }
+
+    public String getAssigned_user_id() {
+        return assigned_user_id;
+    }
+
+    public String getAssigned_user_name() {
+        return assigned_user_name;
+    }
+
+    public String getTeam_count() {
+        return team_count;
+    }
+
+    // Setter Methods
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDate_entered(String date_entered) {
+        this.date_entered = date_entered;
+    }
+
+    public void setDate_modified(String date_modified) {
+        this.date_modified = date_modified;
+    }
+
+    public void setModified_user_id(String modified_user_id) {
+        this.modified_user_id = modified_user_id;
+    }
+
+    public void setModified_by_name(String modified_by_name) {
+        this.modified_by_name = modified_by_name;
+    }
+
+    public void setCreated_by(String created_by) {
+        this.created_by = created_by;
+    }
+
+    public void setCreated_by_name(String created_by_name) {
+        this.created_by_name = created_by_name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public void setModule(String module) {
+        this.module = module;
+    }
+
+    public void setReport_type(String report_type) {
+        this.report_type = report_type;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setIs_published(boolean is_published) {
+        this.is_published = is_published;
+    }
+
+    public void setLast_run_date(String last_run_date) {
+        this.last_run_date = last_run_date;
+    }
+
+    public void setReport_cache_id(String report_cache_id) {
+        this.report_cache_id = report_cache_id;
+    }
+
+    public void setChart_type(String chart_type) {
+        this.chart_type = chart_type;
+    }
+
+    public void setSchedule_type(String schedule_type) {
+        this.schedule_type = schedule_type;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    public void setMy_favorite(boolean my_favorite) {
+        this.my_favorite = my_favorite;
+    }
+
+    public void setSync_key(String sync_key) {
+        this.sync_key = sync_key;
+    }
+
+    public void setAssigned_user_id(String assigned_user_id) {
+        this.assigned_user_id = assigned_user_id;
+    }
+
+    public void setAssigned_user_name(String assigned_user_name) {
+        this.assigned_user_name = assigned_user_name;
+    }
+
+    public void setTeam_count(String team_count) {
+        this.team_count = team_count;
+    }
+}
---
+++ src/main/java/WriteXmlDom1.java
@@ -0,0 +1,118 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.w3c.dom.CDATASection;
+import org.w3c.dom.Comment;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+import java.util.Map;
+import java.util.Scanner;
+import com.github.underscore.U;
+import org.xml.sax.InputSource;
+
+public class WriteXmlDom1 {
+
+    public static void main(String[] args)
+            throws ParserConfigurationException, TransformerException, IOException {
+
+        //read json file.
+        String dataString = "";
+        try {
+            File myObj = new File("//Users///manh//Desktop//DWS_Report//jsonReport//response1.json");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                dataString += myReader.nextLine();
+            }
+            myReader.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        //convert json string to Java class.
+//        ObjectMapper mapper = new ObjectMapper();
+//        JSONWrapper JSONwrap = mapper.readValue(dataString, JSONWrapper.class);
+
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> map
+                = mapper.readValue(dataString, new TypeReference<Map<String,Object>>(){});
+
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            System.out.println("Key:: " + entry.getKey() + " Value : " + entry.getValue());
+        }
+
+        //convert Java class to XML elements.
+
+        //parse to XML.
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        // root elements
+        Document doc = docBuilder.newDocument();
+        Element reportElement = doc.createElement("Report");
+        reportElement.setAttribute("xmlns", "http://soap.sforce.com/2006/04/metadata");
+        doc.appendChild(reportElement);
+        
+        //name
+        createElement(doc, "name",(String)map.get("name"), reportElement);
+        //format
+        createElement(doc, "format",(String)map.get("report_type"), reportElement);
+        //reportType
+        createElement(doc, "reportType",convertName((String)map.get("module")), reportElement);
+        //currency
+        createElement(doc, "currency","GBP", reportElement);
+
+        //create colunm:
+        Element nameElement = doc.createElement(tagName);
+        nameElement.setTextContent(tagValue);
+        parentElement.appendChild(nameElement);
+
+
+        // write dom document to a file
+        try (FileOutputStream output =
+                     new FileOutputStream("//Users///manh//Desktop//DWS_Report//staff-dom.xml")) {
+            writeXml(doc, output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static String convertName(String inputName){
+        String returnString = "";
+        switch (inputName){
+            case "Opportunities":
+                returnString = "Opportunity";
+        }
+        return  returnString;
+
+    }
+    private static void createElement(Document doc, String tagName, String tagValue, Element parentElement){
+        Element nameElement = doc.createElement(tagName);
+        nameElement.setTextContent(tagValue);
+        parentElement.appendChild(nameElement);
+    }
+
+    // write doc to output stream
+    private static void writeXml(Document doc,
+                                 OutputStream output)
+            throws TransformerException {
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+
+        // pretty print
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(output);
+
+        transformer.transform(source, result);
+
+    }
+}
